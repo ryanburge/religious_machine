31b5bb0454bd00dff9db455d124a0d70e42e07b0
Ryan Burge <ryanburge@gmail.com>
2017-02-28 18:42:19 -0600

    made a percent bar chart instead.

--- bar_chart_syntax.R
+++ bar_chart_syntax.R
@@ -4,8 +4,14 @@ reltrad <- data.frame(label, count)
 
 reltrad$label <- factor(reltrad$label, levels=unique(reltrad$label))
 
-ggplot(reltrad, aes(x=label, y=count)) +  geom_col(fill = "coral", colour = "black") +
+reltrad$pct <- reltrad$count/20247
+reltrad$pct <- round(reltrad$pct, 2)
+
+reltrad$pct <- reltrad$pct*100
+
+
+ggplot(reltrad, aes(x=label, y=pct)) +  geom_col(fill = "coral", colour = "black") +
   ggtitle("                                            Distribution of Reltrad") + 
-  xlab("Religious Tradition") + ylab("Number of Respondents")  + 
+  xlab("Religious Tradition") + ylab("Percentage of Respondents")  + 
   theme(text=element_text(size=16, family="KerkisSans")) + 
-  annotate("text", x = 6.65, y = 5250, label = "religioninpublic.blog", size = 5)
+  annotate("text", x = 6.65, y = 25, label = "religioninpublic.blog", size = 5)
